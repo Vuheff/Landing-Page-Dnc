604ec6047da594a13b68cb09bf7fb9214125f4fc
Vuheff <vitor.ferrb@gmail.com>
2026-04-01 02:25:26 -0300

    Sincroniza com origin

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ var Samantha = window.document.getElementById("Samantha")
 var Bruna = window.document.getElementById("Bruna")
 var SetaEsquerda = window.document.getElementById("seta-esquerda")
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> c40eae6 (primeiro commit)
 function RolarParaDireita(){
     Leonardo.style ="display:none"
     Bruna.style ="display:flex"
     SetaEsquerda.style ="display:flex"
     SetaDireita.style ="display:none"
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> c40eae6 (primeiro commit)
 function RolarParaEsquerda(){
     Leonardo.style ="display:flex"
     Bruna.style ="display:none"
     SetaEsquerda.style ="display:none"
     SetaDireita.style ="display:flex"
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> c40eae6 (primeiro commit)
